7f0cf55926433e4c73bcfd5fdeb2bd1f4e87977e
Sebastian Rietzscher <rietzscher@gmail.com>
2021-12-09 16:05:33 +0100

    add: second task of day one solved

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,21 @@
-use std::io::Read;
 
 mod aoc;
 
-fn measure_depth(depth: &[u32]) -> u32 {
+/// Calculates the changes in depth if depth in increased 
+pub fn measure_depth(depth: &[u32]) -> u32 {
     let deep = depth.iter();
     let mut counter = 0;
-    let val = depth.iter().zip(deep.skip(1)).map(|(cur, ne)| if cur < ne { counter +=1 } ).count();
+    depth.iter().zip(deep.skip(1)).map(|(cur, ne)| if cur < ne { counter +=1 } ).count();
     counter 
 }
-fn measure_depth_window(depth: &[u32]) -> u32 {
-    let deep = depth.iter();
+
+
+/// Applies a windowed with a windowsize of 3 to the meassured depth
+pub fn measure_depth_window(depth: &[u32]) -> u32 {
     let mut summed_val : Vec<u32> = Vec::new();
-    while depth.iter().next().is_some(){
-        let val = deep.take(3).sum::<u32>();
-        summed_val.push(val);
-    }
+
+    depth.windows(3).map(|x|summed_val.push(x.iter().sum())).count();
+
     measure_depth(&summed_val)
 }
 
@@ -34,12 +35,17 @@ mod tests {
     #[test]
     fn test_measure_depth_real() {
         let res =  measure_depth(day1::MEASUREMENTS);
-        assert_eq!(res,7);
+        assert_eq!(res,1448);
     }
     #[test]
     fn test_measure_depth_window() {
         let simple_test = [199,200,208,210,200,207,240,269,260,263];
         let res =  measure_depth_window(&simple_test);
-        assert_eq!(res,7);
+        assert_eq!(res,5);
+    }
+    #[test]
+    fn test_measure_depth_window_real() {
+        let res =  measure_depth_window(&day1::MEASUREMENTS);
+        assert_eq!(res,1471);
     }
 }
